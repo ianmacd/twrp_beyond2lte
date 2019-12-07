36f6c049863a8cea3f4bffad4598926d8a2fe2cc
Ian Macdonald <ian@caliban.org>
2019-12-07 15:30:01 +0100

    Version v3.3.1-100_ianmacd.
    Use PLATFORM_VERSION to force ro.build.version.release to reflect
    correct Android version.
    
    Change-Id: I2c1585c4669b33f9d947afbed62e44e45e67e46e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Version 3.3.1-12_ianmacd.
+# Version 3.3.1-100_ianmacd.
 
 # Architecture
 TARGET_ARCH := arm64
@@ -72,4 +72,5 @@ TW_NO_LEGACY_PROPS := true
 BOARD_SUPPRESS_SECURE_ERASE := true
 BOARD_INCLUDE_RECOVERY_DTBO := true
 BOARD_PREBUILT_DTBOIMAGE := device/samsung/beyond2lte/prebuilt/recovery_dtbo
-PLATFORM_SECURITY_PATCH := 2019-11-01
+PLATFORM_VERSION := 10
+PLATFORM_SECURITY_PATCH := 2019-12-01
